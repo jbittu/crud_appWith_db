4131274c7f1e2a276ffa4b5f6b070e7b8764ec1f
jbittu <jaiswalbittu0319@gmail.com>
2025-03-26 20:06:33 +0530

    update from frontend to backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-
 function App() {
   //GET REQUEST
   const [workouts, setWorkouts] = useState(null); //state for geting data from the server
@@ -17,47 +16,116 @@ function App() {
   useEffect(() => {
     getWorkouts();
   }, []);
-//POST REQUEST
-const [form, setForm] = useState({
-  title: "",
-  reps: "",
-  loads: ""
-});
-const handleFormFiel = (e) => {
-  
-  setForm({ ...form, [e.target.name]: e.target.value });
-}
-const createWorkoutData = async (e) => {
-  e.preventDefault();
-  await axios.post("http://localhost:8000/api/workout", form);
-  // console.log(response.data);
-  getWorkouts();
-  setForm({ title: "", reps: "", loads: "" });
-}
-
-//DELETE REQUEST
-const deleteWorkout = async (id) => {
-  try {
-    await axios.delete(`http://localhost:8000/api/workout/${id}`);
-    
+  //POST REQUEST
+  const [form, setForm] = useState({
+    title: "",
+    reps: "",
+    loads: "",
+  });
+  const handleFormFiel = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const createWorkoutData = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8000/api/workout", form);
+    // console.log(response.data);
     getWorkouts();
-  } catch (err) {
-    console.error(err);
-  }
-};
+    setForm({ title: "", reps: "", loads: "" });
+  };
 
+  //DELETE REQUEST
+  const deleteWorkout = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8000/api/workout/${id}`);
+
+      getWorkouts();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  //UPDATE REQUEST
+
+  const [updateForm, setUpdateForm] = useState({
+    _id: null,
+    title: "",
+    reps: "",
+    loads: "",
+  });
+
+  const handleUpdateForm = (e) => {
+    setUpdateForm({ ...updateForm, [e.target.name]: e.target.value });
+  };
+  const toggleUpdate = async (id) => {
+    const response = await axios.get(`http://localhost:8000/api/workout/${id}`);
+    setUpdateForm(response.data);
+  };
+  const updateWorkoutData = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.patch(`http://localhost:8000/api/workout/${updateForm._id}`, updateForm);
+      getWorkouts();
+      setUpdateForm({ _id: null, title: "", reps: "", loads: "" });
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="App">
-      <form onSubmit={createWorkoutData}>
-        <h1>Workout Prep</h1>
-        <label>Title</label>
-        <input type="text" name="title" value={form.title} onChange={handleFormFiel}/>
-        <label>Reps</label>
-        <input type="number" name="reps" value={form.reps} onChange={handleFormFiel}/>
-        <label>Loads</label>
-        <input type="number" name="loads" value={form.loads} onChange={handleFormFiel}/>
-        <button >Submit</button>
-      </form>
+      {!updateForm._id && (
+        <form onSubmit={createWorkoutData}>
+          <h1>Workout Prep</h1>
+          <label>Title</label>
+          <input
+            type="text"
+            name="title"
+            value={form.title}
+            onChange={handleFormFiel}
+          />
+          <label>Reps</label>
+          <input
+            type="number"
+            name="reps"
+            value={form.reps}
+            onChange={handleFormFiel}
+          />
+          <label>Loads</label>
+          <input
+            type="number"
+            name="loads"
+            value={form.loads}
+            onChange={handleFormFiel}
+          />
+          <button>Submit</button>
+        </form>
+      )}
+      {updateForm._id && (
+        <form onSubmit={updateWorkoutData}>
+          <h1> Update Workout Prep</h1>
+          <label>Title</label>
+          <input
+            type="text"
+            name="title"
+            value={updateForm.title}
+            onChange={handleUpdateForm}
+          />
+          <label>Reps</label>
+          <input
+            type="number"
+            name="reps"
+            value={updateForm.reps}
+            onChange={handleUpdateForm}
+          />
+          <label>Loads</label>
+          <input
+            type="number"
+            name="loads"
+            value={updateForm.loads}
+            onChange={handleUpdateForm}
+          />
+          <button>Update Prep</button>
+        </form>
+      )}
       {workouts &&
         workouts.map((item) => {
           return (
@@ -65,6 +133,7 @@ const deleteWorkout = async (id) => {
               <h1>{item.title}</h1>
               <p>{item.reps}</p>
               <p>{item.loads}</p>
+              <button onClick={() => toggleUpdate(item._id)}>Edit</button>{" "}
               <button onClick={() => deleteWorkout(item._id)}>Delete</button>
             </div>
           );
