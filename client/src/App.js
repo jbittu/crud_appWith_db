80cc810e9c65d695f89d138cdfc682e4c504192e
jbittu <jaiswalbittu0319@gmail.com>
2025-03-26 13:31:06 +0530

    get and post from frontend and backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+
 function App() {
+  //GET REQUEST
   const [workouts, setWorkouts] = useState(null); //state for geting data from the server
 
   const getWorkouts = async () => {
@@ -15,9 +17,35 @@ function App() {
   useEffect(() => {
     getWorkouts();
   }, []);
-
+//POST REQUEST
+const [form, setForm] = useState({
+  title: "",
+  reps: "",
+  loads: ""
+});
+const handleFormFiel = (e) => {
+  
+  setForm({ ...form, [e.target.name]: e.target.value });
+}
+const createWorkoutData = async (e) => {
+  e.preventDefault();
+  await axios.post("http://localhost:8000/api/workout", form);
+  // console.log(response.data);
+  getWorkouts();
+  setForm({ title: "", reps: "", loads: "" });
+}
   return (
     <div className="App">
+      <form onSubmit={createWorkoutData}>
+        <h1>Workout Prep</h1>
+        <label>Title</label>
+        <input type="text" name="title" value={form.title} onChange={handleFormFiel}/>
+        <label>Reps</label>
+        <input type="number" name="reps" value={form.reps} onChange={handleFormFiel}/>
+        <label>Loads</label>
+        <input type="number" name="loads" value={form.loads} onChange={handleFormFiel}/>
+        <button >Submit</button>
+      </form>
       {workouts &&
         workouts.map((item) => {
           return (
