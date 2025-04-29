ef9082faf3d5bbf122e78b120f2b56625d14bf51
jbittu <jaiswalbittu0319@gmail.com>
2025-04-29 23:26:47 +0530

    update

--- server/dB/connection.js
+++ server/dB/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose
-  .connect("mongodb://localhost:27017/projectForm")
+  .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log("Connection Successful");
+    console.log("MongoDB Atlas connected successfully");
   })
   .catch((err) => {
-    console.log(`Connection Failed ${err}`);
+    console.log(`MongoDB connection error: ${err}`);
   });
